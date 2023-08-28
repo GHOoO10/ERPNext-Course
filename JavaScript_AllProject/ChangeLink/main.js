8ff9b0881af1c8d2aed96d94a9398c07bc16de95
GHOoO10 <ghamdangithub0@gmail.com>
2023-08-29 00:31:54 +0300

    Uploaded the javascirpt and html files that applied in lectucer two

---
+++ JavaScript_AllProject/ChangeLink/main.js
@@ -0,0 +1,8 @@
+// alert("Hi Gays")
+// document.getElementById("html").click();
+
+
+function ChangLink(){
+    var element = document.getElementById("O");
+    element.setAttribute("href", "https://github.com/GHOoO10");
+}
